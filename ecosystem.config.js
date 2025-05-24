df4bb497e81e510602183fb93f1456f562ae1b30
Alisher Uralov <aliwerdev@gmail.com>
2025-05-24 18:40:16 +0500

    fsd

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     apps: [
         {
-            name: 'nextjs-app',
+            name: 'umft-admission',
             script: 'node_modules/next/dist/bin/next',
             args: 'start -p 3004',
             env: {
